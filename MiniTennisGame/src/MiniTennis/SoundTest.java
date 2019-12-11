490197d00b7d896c0b1b32fcec0a815a8b419c17
MFreds <57950977+MFreds@users.noreply.github.com>
2019-12-11 17:24:07 +0700

    Update

--- MiniTennisGame/src/MiniTennis/SoundTest.java
+++ MiniTennisGame/src/MiniTennis/SoundTest.java
@@ -7,18 +7,6 @@ import java.net.URL;
 public class SoundTest {
 	public static void main(String[] args) throws Exception {
 
-//		System.out.println("1");
-//		URL url = new URL("http://www.edu4java.com/sound/back.wav");
-//		System.out.println("2");
-//		AudioClip clip = Applet.newAudioClip(url);
-//		System.out.println("3");
-//		clip.play();
-//		System.out.println("4");
-//		Thread.sleep(1000);
-
-//		URL url = new URL(
-//			"file:/C:/Users/Eli/workspace/minitennis/src/com/edu4java/minitennis7/back.wav");
-
 		URL url = SoundTest.class.getResource("back.wav");
 		AudioClip clip = Applet.newAudioClip(url);
 		AudioClip clip2 = Applet.newAudioClip(url);
